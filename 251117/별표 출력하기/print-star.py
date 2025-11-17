d442fc9ea2b50d97254a1b2a6d22a50fe9e5bba3
jang won <96022213+Jangonechoi@users.noreply.github.com>
2025-11-17 20:24:49 +0900

    Title: 별표 출력하기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251117/별표 출력하기/print-star.py
@@ -0,0 +1,16 @@
+import sys
+
+N = int(sys.stdin.read())
+cnt = 1
+
+for i in range(2*N-1):
+    for j in range(cnt):
+        print('*', end=' ')
+    print()
+
+    if i < 4:
+        cnt += 1
+    else:
+        cnt -= 1
+
+    
