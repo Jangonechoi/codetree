89881782d22a2822d04d780281b00770c1f7800c
jang won <96022213+Jangonechoi@users.noreply.github.com>
2025-11-18 20:36:17 +0900

    Title: 별표 출력하기 8, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251118/별표 출력하기 8/print-star-8.py
+++ 251118/별표 출력하기 8/print-star-8.py
@@ -1,11 +1,20 @@
 import sys
 
 N = int(sys.stdin.read())
-# print(N)
-for i in range(1,N+1):
-    if i % 2 == 0:
-        print('* '* i)
-    else:
-        print('*')
+# # print(N)
+# for i in range(1,N+1):
+#     if i % 2 == 0:
+#         print('* '* i)
+#     else:
+#         print('*')
+
+#i가 짝수인 경우 별을 1개, 홀수인 경우 i+1개를 출력합니다
+for i in range(N):
+	if i % 2 == 0:
+		print("*", end="")
+	else:
+		for _ in range(i + 1):
+			print("*", end=" ")
+	print()
 
 
