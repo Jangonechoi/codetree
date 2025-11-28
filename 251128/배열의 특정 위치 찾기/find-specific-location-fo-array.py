c631931af2dc1dcc89153a4385b03c5c07638930
jang won <96022213+Jangonechoi@users.noreply.github.com>
2025-11-28 16:32:03 +0900

    Title: 배열의 특정 위치 찾기, Time: 45ms, Memory: 17MB, Status: Passed - Codetree

--- 251128/배열의 특정 위치 찾기/find-specific-location-fo-array.py
+++ 251128/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -2,7 +2,17 @@ import sys
 
 arr = list(map(int, sys.stdin.read().split()))
 # print(arr)
-arr1 = list(filter(lambda x : x % 2 == 0 , arr))
-arr2 = list(filter(lambda x : x % 3 == 0, arr))
-arr2_avg = sum(arr2) / len(arr2)
-print(f"{sum(arr1)} {arr2_avg:.1f}")
+s = 0
+s2 = 0
+s2_len = 0
+
+for i in range(1,len(arr)+1):
+    if i % 2 == 0:
+        s += arr[i-1]
+        # print(arr[i-1])
+    if i % 3 == 0:
+        s2 += arr[i-1]
+        s2_len += 1
+        # print(arr[i-1])
+avg = s2 / s2_len
+print(f'{s} {avg:.1f}')
