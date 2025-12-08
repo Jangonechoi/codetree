d8478d6acf3b92c88ad6e178cf525f28b9264a71
jang won <96022213+Jangonechoi@users.noreply.github.com>
2025-12-08 14:21:21 +0900

    Title: 두 정수에 대한 연산값, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251208/두 정수에 대한 연산값/operational-values-for-two-integers.py
+++ 251208/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -2,5 +2,8 @@ import sys
 a, b = map(int, sys.stdin.read().split())
 
 def solution(x,y):
-    return f"{min(x,y)*2} {max(x,y)+25}"
+    if x < y:
+        return f"{min(x,y)*2} {max(x,y)+25}"
+    else:
+        return f"{max(x,y)+25} {min(x,y)*2}"
 print(solution(a,b))
