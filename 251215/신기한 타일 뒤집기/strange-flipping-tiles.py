0f26ec67598707e886c4251915bbd5a9aa00114a
jang won <96022213+Jangonechoi@users.noreply.github.com>
2025-12-15 15:57:21 +0900

    Title: 신기한 타일 뒤집기, Time: 75ms, Memory: 19MB, Status: Passed - Codetree

--- 251215/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 251215/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -8,7 +8,7 @@ for num, direction in commands:
 
 # Please write your code here.
 # print(n, commands, x, dir) => 4 [('4', 'R'), ('5', 'L'), ('7', 'R'), ('4', 'L')] [4, 5, 7, 4] ['R', 'L', 'R', 'L']
-arr = [0] * 200001 
+arr = [0] * 200001
 offset = 100000
 current_pos = offset
 
@@ -16,14 +16,11 @@ for i in range(n):
     # 이동할 거리 설정
     distance = x[i]
     if dir[i] == 'R':
-        for j in range(current_pos, current_pos + distance):
-            arr[j] = 1
-        # print(arr[99990:100010])
-        current_pos += distance
+        for j in range(distance):
+            arr[current_pos + j] = -1
+        current_pos += distance -1
     else:
-        for j in range(current_pos - distance, current_pos):
-            arr[j] = -1
-        # print(arr[99990:100010])
-        current_pos -= distance
-
-print(arr.count(-1), arr.count(1))
+        for j in range(distance):
+            arr[current_pos - j] = 1
+        current_pos -= distance -1
+print(arr.count(1), arr.count(-1))
