e3a0ef4c4c874171df2661498cb24efc94a94dbe
jang won <96022213+Jangonechoi@users.noreply.github.com>
2025-12-17 11:18:12 +0900

    Title: 벌금은 누구에게, Time: 107ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 251217/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,15 @@
+N, M, K = map(int, input().split())
+student = [int(input()) for _ in range(M)]
+
+# Please write your code here.
+# print(N, M, K, student) 5 7 3 [2, 5, 2, 3, 5, 2, 4]
+arr = [0] * (N+1)
+
+for i in range(M):
+    arr[student[i]] += 1
+    if K in arr:
+        break
+print(arr.index(K))
+
+
+
